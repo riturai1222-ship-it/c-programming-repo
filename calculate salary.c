2cf7f95c4cdce07e563bb210217b6c4b66d1fa26
riturai1222-ship-it <riturai.1222@gmail.com>
2026-07-25 14:28:20 +0530

    Create calculate salary.c

---
+++ calculate salary.c
@@ -0,0 +1,29 @@
+Program to calculate salary based on Gender, Years of Service, and Qualifications
+SOURCE CODE (SALARY_CALCULATOR.C)
+#include <stdio.h>
+int main( )
+{
+char g ;
+int yos, qual, sal = 0 ;
+    printf ( "Enter Gender, Years of Service and Qualifications (0 = G, 1 = PG): " ) ;
+    scanf ( "%c%d%d", &g, &yos, &qual ) ;
+if ( g == 'm' && yos >= 10 && qual == 1 )
+        sal = 11000 ;
+else if ( ( g == 'm' && yos >= 10 && qual == 0 ) ||
+              ( g == 'm' && yos < 10 && qual == 1 ) )
+        sal = 10000 ;
+else if ( g == 'm' && yos < 10 && qual == 0 )
+        sal = 7000 ;
+else if ( g == 'f' && yos >= 10 && qual == 1 )
+        sal = 12000 ;
+else if ( g == 'f' && yos >= 10 && qual == 0 )
+        sal = 9000 ;
+else if ( g == 'f' && yos < 10 && qual == 1 )
+        sal = 10000 ;
+else if ( g == 'f' && yos < 10 && qual == 0 )
+        sal = 6000 ;
+    printf ( "
+Salary of Employee = %d
+", sal ) ;
+return 0 ;
+}
